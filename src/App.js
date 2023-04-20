815c4ee65e81c0503114e2a68f50f69ea88df7fe
jwjb1020 <jwjb1020@gmail.com>
2023-04-20 13:09:46 +0900

    2023.04.20

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+//사용자정의 태그는 무조건!!!! return이 있어야함!!! return 안에는 태그하나!!! (만든 태그에는 자식 얼마든지)
 function App() {
   return (
     <div className="App">
